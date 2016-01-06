95109b98959dfba1b0b2d11b8554e8842f4b5e19
mikemajesty <mikemajesty2011@gmail.com>
2016-01-06 18:27:26 -0200

    Estrutura do Projeto

---
+++ ProjetoModelo.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetoModelo.MVC.Startup))]
+namespace ProjetoModelo.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
