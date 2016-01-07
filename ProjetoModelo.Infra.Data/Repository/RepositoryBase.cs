2223181127a9534692226d60bb485bf67844785b
mikemajesty <mikemajesty2011@gmail.com>
2016-01-06 23:11:56 -0200

    Listar Cliente

--- ProjetoModelo.Infra.Data/Repository/RepositoryBase.cs
+++ ProjetoModelo.Infra.Data/Repository/RepositoryBase.cs
@@ -11,39 +11,39 @@ namespace ProjetoModelo.Infra.Data.Repository
 {
     public class RepositoryBase<TEntity> : IDisposable, IRepositoryBase<TEntity> where TEntity : class
     {
-        protected readonly ProjetoModelContext _context;
-        protected DbSet<TEntity> _dbSet;
-        public RepositoryBase()
-        {
-            _context = new ProjetoModelContext();
-            _dbSet = _context.Set<TEntity>();
-        }
+        protected readonly ProjetoModelContext _context = new ProjetoModelContext();
+       // protected DbSet<TEntity> _dbSet;
+        //public RepositoryBase()
+        //{
+        //    _context = new ProjetoModelContext();
+        //    _dbSet = _context.Set<TEntity>();
+        //}
         public int Add(TEntity entity)
         {
-            _dbSet.Add(entity);
+            _context.Set<TEntity>().Add(entity);
             return SaveChanges();
         }
 
         public void Dispose()
         {
             this._context.Dispose();
-            this._dbSet = null;
+            //this._dbSet = null;
         }
 
         public IEnumerable<TEntity> GetAll()
         {
-           return _dbSet.ToList();
+           return _context.Set<TEntity>().ToList();
         }
 
         public TEntity GetByID(int id)
         {
-            return _dbSet.Find(id);
+            return _context.Set<TEntity>().Find(id);
 
         }
 
         public int Remove(TEntity entity)
         {
-            _dbSet.Remove(entity);
+            _context.Set<TEntity>().Remove(entity);
             return SaveChanges();
         }
 
---
+++ ProjetoModelo.MVC/Controllers/ClientController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjetoModel.Domain;
+using ProjetoModelo.Infra.Data.Repository;
+using AutoMapper;
+using ProjetoModel.Domain.Entities;
+using ProjetoModelo.MVC.ViewModel;
+
+namespace ProjetoModelo.MVC.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly ClientRepository
+            _clientRepository = new ClientRepository();
+        // GET: Client
+        public ActionResult Index()
+        {
+            
+            var clientViewModel = Mapper.Map<IEnumerable<Client>, IEnumerable<ClientViewModel>>(_clientRepository.GetAll());
+            return View(clientViewModel);
+        }
+
+        // GET: Client/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Client/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Client/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Client/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Client/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Client/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Client/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
