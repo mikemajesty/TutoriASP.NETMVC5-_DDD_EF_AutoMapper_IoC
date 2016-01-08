9de1801fda9867dca0ddf39dbeaf4af87b3be6f0
mikemajesty <mikemajesty2011@gmail.com>
2016-01-07 22:57:44 -0200

    Adicionando AppServiceBase

---
+++ ProjetoModelo.Application/AppServiceBase.cs
@@ -0,0 +1,46 @@
+﻿using ProjetoModelo.Application.Interface;
+using System;
+using System.Collections.Generic;
+
+namespace ProjetoModelo.Application
+{
+    public class AppServiceBase<TEntity> : IDisposable, IAppServiceBase<TEntity> where TEntity : class
+    {
+        private readonly IAppServiceBase<TEntity> _serviceBase;
+
+        public AppServiceBase(IAppServiceBase<TEntity> _serviceBase)
+        {
+            this._serviceBase = _serviceBase;
+        }
+
+        public int Add(TEntity entity)
+        {
+            return _serviceBase.Add(entity);
+        }
+
+        public void Dispose()
+        {
+            _serviceBase.Dispose();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _serviceBase.GetAll();
+        }
+
+        public TEntity GetByID(int id)
+        {
+            return _serviceBase.GetByID(id);
+        }
+
+        public int Remove(TEntity entity)
+        {
+            return _serviceBase.Remove(entity);
+        }
+
+        public int Update(TEntity entity)
+        {
+          return  _serviceBase.Update(entity);
+        }
+    }
+}
--- ProjetoModelo.Application/Interface/IProductAppService.cs
+++ ProjetoModelo.Application/Interface/IProductAppService.cs
@@ -1,10 +1,5 @@
 ﻿using ProjetoModel.Domain.Entities;
-using ProjetoModelo.Application.Interface;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProjetoModelo.Application.Interface
 {
